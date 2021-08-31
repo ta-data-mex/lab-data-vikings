26a9d00926c14bcf851656b1802a78792b26f2e5
Mariangel <angelsmreyes@gmail.com>
2021-08-31 18:44:15 -0400

    lab vikings

--- viking_clases.py
+++ viking_clases.py
@@ -2,22 +2,74 @@
 
 
 class Soldier:
-    pass
+    def __init__(self, health, strength):
+        self.health = health
+        self.strength = strength
+        
+    def attack(self):
+        return strength
+    
+    def receive_damage(self, the_damage):
+        health - the_damage
+        
+        
 
 # Viking
 
 
-class Viking:
-    pass
+class Viking(Soldier):
+    def __init__(self, name, health, strength):
+        self.name = name
+        super().__init__(health, strength)
+        
+    def receive_damage(self, the_damage):
+        health - the_damage
+        if health - the_damage > 0:
+            return print(name, " has received ", the_damage, " points of damage")
+        if health - the_damage <= 0:
+            return print (name, " has died in act of combat")
+        
+    def battle_cry(self):
+        print('Odin Owns You All!')
+    
 
 # Saxon
 
 
-class Saxon:
-    pass
+class Saxon(Soldier):
+    def __init__(self, health, strength):
+        super().__init__(health, strength)
+    
+    def receive_damage(self, the_damage):
+        health - the_damage
+        if health - the_damage > 0:
+            return print('A Saxon has received ',the_damage, ' points of damage')
+        if health - the_damage <= 0:
+            return print('A Saxon has died in combat')
+    
 
 # War
 
 
 class War:
-pass
+    def __init__(self):
+        viking_army = []
+        saxon_army = []
+        
+    def add_viking(Viking):
+        viking_army.append(viking)
+        
+    def add_saxon(Saxon):
+        saxon_army.append(Saxon)
+        
+    def viking_attack(self):
+         Saxon.receive_damage(Viking.strength)
+            if Saxon.health <= 0:
+                saxon_army.remove(Saxon)
+        
+        
+        
+        
+        
+        
+        
